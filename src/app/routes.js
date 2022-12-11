b26bc35c2c1b5eb78af028c3433f3ed7c78abfaa
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-12-11 11:33:26 +0800

    completed dashboard features

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,25 +1,25 @@
-import * as React from 'react'
-import { useRoutes } from 'react-router-dom'
-import DashBoard from '../pages/DashBoard'
-import LoginPage from '../pages/LoginPage'
-import MainPage from '../pages/MainPage'
+import * as React from "react";
+import { useRoutes } from "react-router-dom";
+import DashBoard from "../pages/DashBoard";
+import LoginPage from "../pages/LoginPage";
+import MainPage from "../pages/MainPage";
 
 export default function Routes() {
   let element = useRoutes([
     {
-      path: '/',
+      path: "/",
       element: <LoginPage />,
     },
     {
       element: <MainPage />,
       children: [
         {
-          path: '/dashboard',
+          path: "/dashboard",
           element: <DashBoard />,
         },
       ],
     },
-  ])
+  ]);
 
-  return element
+  return element;
 }
--- src/pages/DashBoard.js
+++ src/pages/DashBoard.js
@@ -1,34 +1,34 @@
-import React, { useEffect, useState } from 'react'
-import { storage } from '../app/db'
-import Skeleton from '@mui/material/Skeleton'
+import React, { useEffect, useState } from "react";
+import { storage } from "../app/db";
+import Skeleton from "@mui/material/Skeleton";
 import {
   ref,
   listAll,
   getDownloadURL,
   getMetadata,
   deleteObject,
-} from 'firebase/storage'
-import { DataGrid } from '@mui/x-data-grid'
+} from "firebase/storage";
+import { DataGrid } from "@mui/x-data-grid";
 
-import DynamicTable from '../components/DynamicTable'
+import DynamicTable from "../components/DynamicTable";
 
 export default function DashBoard() {
-  const [files, setFiles] = useState([])
-  const [loading, setLoading] = useState(false)
+  const [files, setFiles] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const getAll = async () => {
-    setLoading(true)
-    const listRef = ref(storage)
-    const resList = await listAll(listRef)
-    let items = []
+    setLoading(true);
+    const listRef = ref(storage);
+    const resList = await listAll(listRef);
+    let items = [];
 
     await Promise.all(
       resList.items.map(async (item) => {
-        console.log(item)
-        const { name } = item
-        const url = await getDownloadURL(item)
-        const meta = await getMetadata(item)
-        console.log(meta)
+        console.log(item);
+        const { name } = item;
+        const url = await getDownloadURL(item);
+        const meta = await getMetadata(item);
+        console.log(meta);
         items.push({
           id: meta.generation,
           name: name,
@@ -36,64 +36,97 @@ export default function DashBoard() {
           size: meta.size,
           type: meta.contentType,
           lastUpdated: meta.updated,
-        })
-      }),
-    )
-    setLoading(false)
-    return items
-  }
+        });
+      })
+    );
+    setLoading(false);
+    return items;
+  };
 
   const getAllImage = async () => {
-    setLoading(true)
-    const files = await getAll()
-    const filtered = files.filter((file) => file['type'].includes('image/png'))
-    setFiles(filtered)
-    setLoading(false)
-  }
+    setLoading(true);
+    const files = await getAll();
+    const filtered = files.filter((file) => file["type"].includes("image"));
+    setFiles(filtered);
+    setLoading(false);
+  };
+
+  const getAllVideos = async () => {
+    setLoading(true);
+    const files = await getAll();
+    const filtered = files.filter((file) => file["type"].includes("video"));
+    setFiles(filtered);
+    setLoading(false);
+  };
+
+  const getAllAudio = async () => {
+    setLoading(true);
+    const files = await getAll();
+    const filtered = files.filter((file) => file["type"].includes("audio"));
+    setFiles(filtered);
+    setLoading(false);
+  };
+
+  const getAllDocuments = async () => {
+    setLoading(true);
+    const files = await getAll();
+    const filtered = files.filter((file) =>
+      file["type"].includes("application")
+    );
+    setFiles(filtered);
+    setLoading(false);
+  };
+
+  const getAllFiles = async () => {
+    setLoading(true);
+    const files = await getAll();
+    setFiles(files);
+    setLoading(false);
+  };
 
   useEffect(() => {
-    ;(async () => {
-      const datas = await getAll()
-      setFiles(datas)
-    })()
-  }, [])
+    (async () => {
+      const datas = await getAll();
+      setFiles(datas);
+    })();
+  }, []);
 
   const columns = [
-    { field: 'name', headerName: 'File Name', width: 150 },
-    { field: 'lastUpdated', headerName: 'last Updated', width: 150 },
-    { field: 'size', headerName: 'Size', width: 150 },
+    { field: "name", headerName: "File Name", width: 150 },
+    { field: "lastUpdated", headerName: "last Updated", width: 150 },
+    { field: "size", headerName: "Size", width: 150 },
     {
-      field: 'action',
-      headerName: 'Action',
+      field: "action",
+      headerName: "Action",
       width: 150,
       renderCell: ({ row }) => (
         <button onClick={() => handleDelete(row.name)}>Delete</button>
       ),
     },
-  ]
+  ];
   const handleDelete = (name) => {
-    const itemRef = ref(storage, name)
+    const itemRef = ref(storage, name);
 
     deleteObject(itemRef)
       .then(() => {
-        console.log('Deleted Successfully')
-        return getAll()
+        console.log("Deleted Successfully");
+        return getAll();
       })
       .then((files) => setFiles(files))
-      .catch((err) => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
   if (files)
     return (
-      <div style={{ height: '60vh', width: '50%' }}>
-        <button>All</button>
-        <button>Audios</button>
-        <button>Documents</button>
+      <div style={{ height: "60vh", width: "50%" }}>
+        <button onClick={getAllFiles}>All</button>
+        <button onClick={getAllAudio}>Audios</button>
+        <button onClick={getAllDocuments}>Documents</button>
         <button onClick={getAllImage}>Images</button>
-        <button>Videos</button>
+        <button onClick={getAllVideos}>Videos</button>
         <DataGrid rows={files} columns={columns} loading={loading} />
       </div>
-    )
+    );
 
-  return <Skeleton variant="rectangular" width={210} height={118} />
+  return <Skeleton variant="rectangular" width={210} height={118} />;
 }
