3507bca20145fa83ffdf10a82f34b6f9bc35c094
Ludwig Ivan Comiso <comisoivan@gmail.com>
2023-01-11 19:59:03 +0800

    final

--- src/features/account/accountSlice.js
+++ src/features/account/accountSlice.js
@@ -1,82 +1,82 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import { auth } from '../../app/db'
-import { signInWithEmailAndPassword, signOut } from 'firebase/auth'
-import { translationFirebaseErrorsEN } from 'react-translation-firebase-errors'
-import jwt from 'jwt-decode'
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { auth } from "../../app/db";
+import { signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { translationFirebaseErrorsEN } from "react-translation-firebase-errors";
+import jwt from "jwt-decode";
 
 const initialState = {
   isAdmin: false,
   isLoggedIn: false,
   isLoading: false,
-  token: localStorage.getItem('token'),
-}
+  token: localStorage.getItem("token"),
+};
 
 export const loginAsync = createAsyncThunk(
-  'accout/login',
+  "accout/login",
   async ({ username, password }, { rejectWithValue }) => {
     try {
       const credentials = await signInWithEmailAndPassword(
         auth,
         username,
-        password,
-      )
-      const { accessToken } = credentials.user
-      localStorage.setItem('token', accessToken)
-      const { email } = jwt(accessToken)
-      const isAdmin = String(email).includes('@admin')
-      console.log(email)
-      return { email, accessToken, isAdmin }
+        password
+      );
+      const { accessToken } = credentials.user;
+      localStorage.setItem("token", accessToken);
+      const { email } = jwt(accessToken);
+      const isAdmin = String(email).includes("@admin");
+      console.log(email);
+      return { email, accessToken, isAdmin };
     } catch (err) {
       if (err.code) {
-        const error = translationFirebaseErrorsEN(err.code)
-        return rejectWithValue(error)
+        const error = translationFirebaseErrorsEN(err.code);
+        return rejectWithValue(error);
       }
-      return rejectWithValue(err.response.data)
+      return rejectWithValue(err.response.data);
     }
-  },
-)
+  }
+);
 
-export const logoutAsync = createAsyncThunk('account/logout', async () => {
-  localStorage.removeItem('token')
-  await signOut(auth)
-})
+export const logoutAsync = createAsyncThunk("account/logout", async () => {
+  localStorage.removeItem("token");
+  await signOut(auth);
+});
 
 const accountSlice = createSlice({
-  name: 'account',
+  name: "account",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(loginAsync.pending, (state) => {
-        state.isLoggedIn = false
-        state.isLoading = true
+        state.isLoggedIn = false;
+        state.isLoading = true;
       })
       .addCase(loginAsync.fulfilled, (state, action) => {
-        state.isAdmin = action.payload.isAdmin
-        state.token = action.payload.accessToken
-        state.isLoggedIn = true
-        state.isLoading = false
+        state.isAdmin = action.payload.isAdmin;
+        state.token = action.payload.accessToken;
+        state.isLoggedIn = true;
+        state.isLoading = false;
       })
       .addCase(loginAsync.rejected, (state) => {
-        state.isLoggedIn = false
-        state.isLoading = false
+        state.isLoggedIn = false;
+        state.isLoading = false;
       })
 
       .addCase(logoutAsync.pending, (state) => {
-        state.isLoggedIn = true
-        state.isLoading = true
+        state.isLoggedIn = true;
+        state.isLoading = true;
       })
       .addCase(logoutAsync.fulfilled, (state, action) => {
-        state.isAdmin = false
-        state.token = ''
-        state.isLoggedIn = false
-        state.isLoading = false
+        state.isAdmin = false;
+        state.token = "";
+        state.isLoggedIn = false;
+        state.isLoading = false;
       })
       .addCase(logoutAsync.rejected, (state) => {
-        state.isLoggedIn = true
-        state.isLoading = false
-      })
+        state.isLoggedIn = true;
+        state.isLoading = false;
+      });
   },
-})
+});
 
-export default accountSlice.reducer
+export default accountSlice.reducer;
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,52 +1,56 @@
-import React, { useEffect } from 'react'
-import { Carousel, Image } from 'antd'
+import React, { useEffect } from "react";
+import { Carousel, Image } from "antd";
 
-import './About.css'
+import "./About.css";
 
 const contentStyle = {
   margin: 0,
-  height: '100vh',
-  color: '#fff',
-  background: 'transparent',
-  position: 'relative',
-  top: '20em',
-}
+  height: "100vh",
+  color: "#fff",
+  background: "transparent",
+  position: "relative",
+  top: "20em",
+};
 
 export default function AboutPage() {
   useEffect(() => {
-    document.body.classList.add('info-body')
+    document.body.classList.add("info-body");
 
-    return () => document.body.classList.remove('info-body')
-  })
+    return () => document.body.classList.remove("info-body");
+  });
 
   return (
     <Carousel
       autoplay
       autoplaySpeed={3000}
-      dotPosition={'left'}
+      dotPosition={"left"}
       effect="fade"
       style={{
-        height: '80vh',
-        width: '90vw',
-        position: 'absolute',
+        height: "80vh",
+        width: "90vw",
+        position: "absolute",
       }}
     >
       <div>
-        <div style={{ ...contentStyle, marginLeft: '15%' }}>
-          <h2 className="about-name">About Us</h2>
-          <p className="about-description" align="justify">
+        <div style={{ ...contentStyle, marginLeft: "15%" }}>
+          <h2 className="about-name-main">About Us</h2>
+          <p className="about-description-main" align="justify">
             <br />
-            Piedmont, or mountain, glaciers are found in many parts of the
-            world. In North America they are distributed along the mountain
-            ranges of the Pacific Coast from central California northward. They
-            abound in the Andes range in South America and are familiar and
-            greatly admired spectacles in the Alps, the Pyrenees, the Caucasus
-            Mountains and the mountains of Scandanavia. Rivers of ice flow down
-            the valleys of various Asian mountain ranges, including the
-            Himalayas, the Hindu Kush, and the Karakoram and Kunlun ranges. They
-            are also a feature of the Southern Alps of New Zealand and are found
-            in the lofty mountains of New Guinea. The largest piedmont glaciers
-            are the Malaspina and Bering glaciers, both in Alaska.
+            UpBox is Cloud storage provider that offers Storage as a Service or
+            STaaS. This model of service is where the cloud storage that allows
+            you to rent from a Cloud Service Provider (UpBox) and that provides
+            basic ways to access that storage. The main advantage of STaaS is
+            that you may outsource to a different CSP the expense and work of
+            managing the infrastructure and technology for data storage. This
+            increases the efficiency of scaling up storage resources without
+            spending money on new hardware or incurring configuration fees. This
+            research intends to provide Poblacion Sabang, Barangay 10, Lipa City
+            with a secure cloud storage system. This type of project proposal
+            would supply the Barangay with cloud storage so that documents,
+            files, CCTV footage, and other data can be conveniently stored and
+            backed up whenever problems arise. The purpose of UpBox is to enable
+            storage more efficiently, particularly in Poblacion Sabang, Barangay
+            10, Lipa City, by securing files, records, and other data.
           </p>
           {/* <img
             className="about-image"
@@ -57,88 +61,59 @@ export default function AboutPage() {
       </div>
       <div>
         <div style={contentStyle}>
-          <h2 className="about-name">John Doe</h2>
+          <h2 className="about-name">Iñigo Miguel D. Pernez</h2>
           <p className="about-description" align="justify">
             <br />
-            Generating random paragraphs can be an excellent way for writers to
-            get their creative flow going at the beginning of the day. The
-            writer has no idea what topic the random paragraph will be about
-            when it appears. This forces the writer to use creativity to
-            complete one of three common writing challenges. The writer can use
-            the paragraph as the first one of a short story and build upon it. A
-            second option is to use the random paragraph somewhere in a short
-            story they create. The third option is to have the random paragraph
-            be the ending paragraph in a short story. No matter which of these
-            challenges is undertaken, the writer is forced to use creativity to
-            incorporate the paragraph into their writing.
+            IT4A - Information Technology
           </p>
-          <img
-            className="about-image"
-            alt="asddd"
-            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-          />
+          <img className="about-image" alt="asddd" src="/images/inigo.jpg" />
         </div>
       </div>
       <div>
         <div style={contentStyle}>
-          <h2 className="about-name">Ben Stiler</h2>
+          <h2 className="about-name">Lawrenz Anthony L. Tugade</h2>
           <p className="about-description" align="justify">
-            <br />A random paragraph can also be an excellent way for a writer
-            to tackle writers' block. Writing block can often happen due to
-            being stuck with a current project that the writer is trying to
-            complete. By inserting a completely random paragraph from which to
-            begin, it can take down some of the issues that may have been
-            causing the writers' block in the first place.
+            <br />
+            IT4A - Information Technology
           </p>
           <img
             className="about-image"
             alt="asddd"
-            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+            src="/images/lanz.jpg"
             width={300}
           />
         </div>
       </div>
       <div>
         <div style={contentStyle}>
-          <h2 className="about-name">Toby Mcguire</h2>
+          <h2 className="about-name">Kyer B. Regacho</h2>
           <p className="about-description" align="justify">
             <br />
-            Another productive way to use this tool to begin a daily writing
-            routine. One way is to generate a random paragraph with the
-            intention to try to rewrite it while still keeping the original
-            meaning. The purpose here is to just get the writing started so that
-            when the writer goes onto their day's writing projects, words are
-            already flowing from their fingers.
+            IT4A - Information Technology
           </p>
           <img
             className="about-image"
             alt="asddd"
-            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+            src="/images/kyer.jpg"
             width={300}
           />
         </div>
       </div>
       <div>
         <div style={contentStyle}>
-          <h2 className="about-name">Lanna Rae</h2>
+          <h2 className="about-name">Rondavid R. Delarosa</h2>
           <p className="about-description" align="justify">
             <br />
-            Another writing challenge can be to take the individual sentences in
-            the random paragraph and incorporate a single sentence from that
-            into a new paragraph to create a short story. Unlike the random
-            sentence generator, the sentences from the random paragraph will
-            have some connection to one another so it will be a bit different.
-            You also won't know exactly how many sentences will appear in the
-            random paragraph.
+            IT4A - Information Technology
           </p>
           <img
             className="about-image"
             alt="asddd"
-            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+            src="/images/lawrenz.jpg"
             width={300}
           />
         </div>
       </div>
     </Carousel>
-  )
+  );
 }
