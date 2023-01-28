8da742bc88298f6641a7ddc8ef1830dc1f9b5898
Ludwig Ivan Comiso <comisoivan@gmail.com>
2023-01-28 16:55:42 +0800

    worker on trash

--- src/pages/Trash.js
+++ src/pages/Trash.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import { storage, db } from '../app/db'
-import Skeleton from '@mui/material/Skeleton'
+import React, { useEffect, useState } from "react";
+import { storage, db } from "../app/db";
+import Skeleton from "@mui/material/Skeleton";
 import {
   doc,
   collection,
@@ -9,9 +9,10 @@ import {
   getDocs,
   updateDoc,
   deleteDoc,
-} from 'firebase/firestore'
-import { toast } from 'react-toastify'
-import { translationFirebaseErrorsEN } from 'react-translation-firebase-errors'
+} from "firebase/firestore";
+import { toast } from "react-toastify";
+import { translationFirebaseErrorsEN } from "react-translation-firebase-errors";
+import { useSelector } from "react-redux";
 
 import {
   ref,
@@ -19,44 +20,48 @@ import {
   getDownloadURL,
   getMetadata,
   deleteObject,
-} from 'firebase/storage'
-import { DataGrid } from '@mui/x-data-grid'
-import { Button } from '@mui/material'
-import IconButton from '@mui/material/IconButton'
-import RestoreIcon from '@mui/icons-material/Restore'
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
-import ButtonGroup from '@mui/material/ButtonGroup'
-import Toast from '../components/Toast'
+} from "firebase/storage";
+import { DataGrid } from "@mui/x-data-grid";
+import { Button } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import RestoreIcon from "@mui/icons-material/Restore";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Toast from "../components/Toast";
+import useEmail from "../hooks/useEmail";
+import { audit } from "../helper/worker";
 
 export default function Trash() {
-  const [type, setType] = useState(0)
-  const [files, setFiles] = useState([])
-  const [loading, setLoading] = useState(false)
+  const { token } = useSelector((state) => state.account);
+  const { email } = useEmail(token);
+  const [type, setType] = useState(0);
+  const [files, setFiles] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const getAll = async () => {
     try {
-      setLoading(true)
+      setLoading(true);
       const list = query(
-        collection(db, 'files'),
-        where('isDeleted', '==', true),
-      )
-      const querySnapshot = await getDocs(list)
-      let datas = []
+        collection(db, "files"),
+        where("isDeleted", "==", true)
+      );
+      const querySnapshot = await getDocs(list);
+      let datas = [];
       querySnapshot.forEach((item) =>
-        datas.push({ id: item.id, name: item.data().filename }),
-      )
+        datas.push({ id: item.id, name: item.data().filename })
+      );
 
-      const listRef = ref(storage)
-      const resList = await listAll(listRef)
-      let items = []
+      const listRef = ref(storage);
+      const resList = await listAll(listRef);
+      let items = [];
 
       await Promise.all(
         resList.items.map(async (item) => {
-          const { name } = item
+          const { name } = item;
           if (datas.some((data) => data.name === name)) {
-            const { id } = datas.find((data) => data.name === name)
-            const url = await getDownloadURL(item)
-            const meta = await getMetadata(item)
+            const { id } = datas.find((data) => data.name === name);
+            const url = await getDownloadURL(item);
+            const meta = await getMetadata(item);
             items.push({
               id: id,
               name: name,
@@ -64,229 +69,331 @@ export default function Trash() {
               size: meta.size,
               type: meta.contentType,
               lastUpdated: meta.updated,
-            })
+            });
           }
-        }),
-      )
-      setLoading(false)
-      return items
+        })
+      );
+      setLoading(false);
+      return items;
     } catch (err) {
-      const error = translationFirebaseErrorsEN(err.code)
-      toast(error, { type: 'error' })
+      const error = translationFirebaseErrorsEN(err.code);
+      toast(error, { type: "error" });
     }
-  }
+  };
 
   const getAllImage = async () => {
     try {
-      setLoading(true)
-      const files = await getAll()
-      const filtered = files.filter((file) => file['type'].includes('image'))
-      setFiles(filtered)
-      setType(1)
-      setLoading(false)
+      setLoading(true);
+      const files = await getAll();
+      const filtered = files.filter((file) => file["type"].includes("image"));
+      setFiles(filtered);
+      setType(1);
+
+      await audit({
+        user: email,
+        activity: "Viewing",
+        date: Date.now(),
+        description: "Viewing Images on Trash",
+        priority: "Low",
+        status: "Success",
+      });
+
+      setLoading(false);
     } catch (err) {
-      const error = translationFirebaseErrorsEN(err.code)
-      toast(error, { type: 'error' })
+      await audit({
+        user: email,
+        activity: "Viewing",
+        date: Date.now(),
+        description: "Viewing Images on Trash",
+        priority: "Low",
+        status: "Failed",
+      });
+      const error = translationFirebaseErrorsEN(err.code);
+      toast(error, { type: "error" });
     }
-  }
+  };
 
   const getAllVideos = async () => {
     try {
-      setLoading(true)
-      const files = await getAll()
-      const filtered = files.filter((file) => file['type'].includes('video'))
-      setFiles(filtered)
-      setType(2)
-      setLoading(false)
+      setLoading(true);
+      const files = await getAll();
+      const filtered = files.filter((file) => file["type"].includes("video"));
+      setFiles(filtered);
+      setType(2);
+
+      await audit({
+        user: email,
+        activity: "Viewing",
+        date: Date.now(),
+        description: "Viewing Videos on Trash",
+        priority: "Low",
+        status: "Success",
+      });
+
+      setLoading(false);
     } catch (err) {
-      const error = translationFirebaseErrorsEN(err.code)
-      toast(error, { type: 'error' })
+      await audit({
+        user: email,
+        activity: "Viewing",
+        date: Date.now(),
+        description: "Viewing Videos on Trash",
+        priority: "Low",
+        status: "Failed",
+      });
+      const error = translationFirebaseErrorsEN(err.code);
+      toast(error, { type: "error" });
     }
-  }
+  };
 
   const getAllAudio = async () => {
     try {
-      setLoading(true)
-      const files = await getAll()
-      const filtered = files.filter((file) => file['type'].includes('audio'))
-      setFiles(filtered)
-      setType(3)
-      setLoading(false)
+      setLoading(true);
+      const files = await getAll();
+      const filtered = files.filter((file) => file["type"].includes("audio"));
+      setFiles(filtered);
+      setType(3);
+
+      await audit({
+        user: email,
+        activity: "Viewing",
+        date: Date.now(),
+        description: "Viewing Audios on Trash",
+        priority: "Low",
+        status: "Success",
+      });
+
+      setLoading(false);
     } catch (err) {
-      const error = translationFirebaseErrorsEN(err.code)
-      toast(error, { type: 'error' })
+      await audit({
+        user: email,
+        activity: "Viewing",
+        date: Date.now(),
+        description: "Viewing Audios on Trash",
+        priority: "Low",
+        status: "Failed",
+      });
+      const error = translationFirebaseErrorsEN(err.code);
+      toast(error, { type: "error" });
     }
-  }
+  };
 
   const getAllDocuments = async () => {
     try {
-      setLoading(true)
-      const files = await getAll()
+      setLoading(true);
+      const files = await getAll();
       const filtered = files.filter((file) =>
-        file['type'].includes('application'),
-      )
-      setFiles(filtered)
-      setType(4)
-      setLoading(false)
+        file["type"].includes("application")
+      );
+      setFiles(filtered);
+      setType(4);
+
+      await audit({
+        user: email,
+        activity: "Viewing",
+        date: Date.now(),
+        description: "Viewing Documents on Trash",
+        priority: "Low",
+        status: "Success",
+      });
+
+      setLoading(false);
     } catch (err) {
-      const error = translationFirebaseErrorsEN(err.code)
-      toast(error, { type: 'error' })
+      await audit({
+        user: email,
+        activity: "Viewing",
+        date: Date.now(),
+        description: "Viewing Documents on Trash",
+        priority: "Low",
+        status: "Failed",
+      });
+      const error = translationFirebaseErrorsEN(err.code);
+      toast(error, { type: "error" });
     }
-  }
+  };
 
   const getAllFiles = async () => {
     try {
-      setLoading(true)
-      const files = await getAll()
-      setFiles(files)
-      setType(0)
-      setLoading(false)
+      setLoading(true);
+      const files = await getAll();
+      setFiles(files);
+      setType(0);
+      setLoading(false);
     } catch (err) {
-      const error = translationFirebaseErrorsEN(err.code)
-      toast(error, { type: 'error' })
+      const error = translationFirebaseErrorsEN(err.code);
+      toast(error, { type: "error" });
     }
-  }
+  };
 
   useEffect(() => {
-    ;(async () => {
-      const datas = await getAll()
-      setFiles(datas)
-    })()
-  }, [])
+    (async () => {
+      const datas = await getAll();
+      setFiles(datas);
+    })();
+    return async () => {
+      await audit({
+        user: email,
+        activity: "Viewing",
+        date: Date.now(),
+        description: "Viewing on Trash",
+        priority: "Low",
+        status: "Success",
+      });
+    };
+  }, []);
 
   const columns = [
     {
-      field: 'id',
-      headerName: 'Id',
+      field: "id",
+      headerName: "Id",
       flex: 1,
       minWidth: 200,
-      headerClassName: 'super-app-theme--header',
+      headerClassName: "super-app-theme--header",
     },
     {
-      field: 'name',
-      headerName: 'File Name',
+      field: "name",
+      headerName: "File Name",
       flex: 1,
       minWidth: 275,
-      headerClassName: 'super-app-theme--header',
+      headerClassName: "super-app-theme--header",
     },
     {
-      field: 'lastUpdated',
-      headerName: 'Last Updated',
-      headerClassName: 'super-app-theme--header',
+      field: "lastUpdated",
+      headerName: "Last Updated",
+      headerClassName: "super-app-theme--header",
       width: 150,
       valueGetter: (params) => {
-        let newDate = new Date(params.row.lastUpdated)
+        let newDate = new Date(params.row.lastUpdated);
         return (
           newDate.getMonth() +
           1 +
-          '/' +
+          "/" +
           newDate.getDay() +
-          '/' +
+          "/" +
           newDate.getFullYear() +
-          ' ' +
+          " " +
           newDate.getHours() +
-          ':' +
+          ":" +
           newDate.getMinutes()
-        )
+        );
       },
     },
     {
-      field: 'size',
-      headerName: 'Size',
-      headerClassName: 'super-app-theme--header',
+      field: "size",
+      headerName: "Size",
+      headerClassName: "super-app-theme--header",
       width: 125,
       valueGetter: (params) => {
-        const size = params.row.size
-        if (!+size) return '0 Bytes'
+        const size = params.row.size;
+        if (!+size) return "0 Bytes";
 
-        const k = 1024
-        const dm = 2 < 0 ? 0 : 2
-        const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+        const k = 1024;
+        const dm = 2 < 0 ? 0 : 2;
+        const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
 
-        const i = Math.floor(Math.log(size) / Math.log(k))
+        const i = Math.floor(Math.log(size) / Math.log(k));
 
-        return `${parseFloat((size / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
+        return `${parseFloat((size / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
       },
     },
     {
-      field: 'restore',
-      headerName: 'Restore',
-      headerClassName: 'super-app-theme--header',
+      field: "restore",
+      headerName: "Restore",
+      headerClassName: "super-app-theme--header",
       width: 100,
-      headerAlign: 'center',
-      align: 'center',
+      headerAlign: "center",
+      align: "center",
       renderCell: ({ row }) => (
-        <IconButton onClick={() => handleRestore(row.id)} size="small">
+        <IconButton
+          onClick={() => handleRestore(row.id, row.name)}
+          size="small"
+        >
           <RestoreIcon color="primary" />
         </IconButton>
       ),
     },
     {
-      field: 'delete',
-      headerName: 'Delete',
-      headerClassName: 'super-app-theme--header',
+      field: "delete",
+      headerName: "Delete",
+      headerClassName: "super-app-theme--header",
       width: 100,
-      headerAlign: 'center',
-      align: 'center',
+      headerAlign: "center",
+      align: "center",
       renderCell: ({ row }) => (
         <IconButton onClick={() => handleDelete(row.id, row.name)} size="small">
-          <DeleteForeverIcon sx={{ color: '#c92216' }} />
+          <DeleteForeverIcon sx={{ color: "#c92216" }} />
         </IconButton>
       ),
     },
-  ]
-  const handleRestore = (id) => {
-    const ref = doc(db, 'files', id)
+  ];
+  const handleRestore = (id, name) => {
+    const ref = doc(db, "files", id);
     updateDoc(ref, { isDeleted: false })
       .then(() => {
-        console.log('Restore Successful!')
+        console.log("Restore Successful!");
         if (type === 1) {
-          getAllImage()
+          getAllImage();
         } else if (type === 2) {
-          getAllVideos()
+          getAllVideos();
         } else if (type === 3) {
-          getAllAudio()
+          getAllAudio();
         } else if (type === 4) {
-          getAllDocuments()
+          getAllDocuments();
         } else {
-          getAllFiles()
+          getAllFiles();
         }
       })
-      .catch((err) => console.log(err))
-  }
+      .then(() => {
+        audit({
+          user: email,
+          activity: "Restore",
+          date: Date.now(),
+          description: `Restored ${name} on Trash`,
+          priority: "Low",
+          status: "Success",
+        });
+      })
+      .catch((err) => console.log(err));
+  };
 
   const handleDelete = async (id, name) => {
-    const dataRef = doc(db, 'files', id)
-    const itemRef = ref(storage, name)
+    const dataRef = doc(db, "files", id);
+    const itemRef = ref(storage, name);
 
-    await deleteDoc(dataRef, { isDeleted: false })
-    await deleteObject(itemRef)
+    await deleteDoc(dataRef, { isDeleted: false });
+    await deleteObject(itemRef);
 
     if (type === 1) {
-      getAllImage()
+      getAllImage();
     } else if (type === 2) {
-      getAllVideos()
+      getAllVideos();
     } else if (type === 3) {
-      getAllAudio()
+      getAllAudio();
     } else if (type === 4) {
-      getAllDocuments()
+      getAllDocuments();
     } else {
-      getAllFiles()
+      getAllFiles();
     }
 
-    console.log('Delete Successful!')
-  }
+    await audit({
+      user: email,
+      activity: "Delete",
+      date: Date.now(),
+      description: `Deleted ${name} on Trash`,
+      priority: "Low",
+      status: "Success",
+    });
+  };
 
   if (files)
     return (
       <>
-        <div style={{ position: 'absolute' }}>
+        <div style={{ position: "absolute" }}>
           <p
             style={{
-              fontFamily: 'Roboto',
-              fontSize: '20px',
+              fontFamily: "Roboto",
+              fontSize: "20px",
               fontWeight: 700,
-              color: '#1565c0',
+              color: "#1565c0",
             }}
           >
             Trash
@@ -294,11 +401,11 @@ export default function Trash() {
         </div>
         <div
           style={{
-            height: '60vh',
-            width: '70vw',
+            height: "60vh",
+            width: "70vw",
           }}
         >
-          <div style={{ transform: 'translate(5em, 3em)' }}>
+          <div style={{ transform: "translate(5em, 3em)" }}>
             <ButtonGroup
               variant="contained"
               aria-label="outlined primary button group"
@@ -318,11 +425,11 @@ export default function Trash() {
             loading={loading}
             size="small"
             sx={{
-              boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',
-              fontFamily: 'Work Sans',
-              transform: 'translate(5em, 5em)',
-              '& .super-app-theme--header': {
-                fontFamily: 'Roboto',
+              boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+              fontFamily: "Work Sans",
+              transform: "translate(5em, 5em)",
+              "& .super-app-theme--header": {
+                fontFamily: "Roboto",
                 fontWeight: 700,
               },
             }}
@@ -330,7 +437,7 @@ export default function Trash() {
         </div>
         <Toast />
       </>
-    )
+    );
 
-  return <Skeleton variant="rectangular" width={210} height={118} />
+  return <Skeleton variant="rectangular" width={210} height={118} />;
 }
